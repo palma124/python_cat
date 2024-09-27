0246497c6a6878421abf493d9e8436194dd0feb4
palma124 <muyaphilip16@gmail.com>
2024-09-27 10:07:34 +0300

    python cat

---
+++ add_num.py
@@ -0,0 +1,9 @@
+def sum(num1,num2):
+    return num1 + num2
+
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+
+result= sum(num1, num2)
+
+print(f"The sum of {num1} and {num2} is {result}")
---
+++ square.py
@@ -0,0 +1,6 @@
+def square(num):
+    return num ** 2
+number= int(input("Enter the number: "))
+result= square(number)
+
+print(f"The square of {number} is: {result}")
